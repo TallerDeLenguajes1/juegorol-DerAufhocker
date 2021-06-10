d87343be9bf3fd7116ce30d81d7bd8ec2ef88331
Exequiel Diaz <79394777+DerAufhocker@users.noreply.github.com>
2021-06-10 17:33:58 -0300

    trate de hacerlo con graficos pero no funciono asi que comence a hacer la logica antes de hacer la parte grafica

---
+++ Juego09/Juego09/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego09
+{
+    public partial class Form1 : Form
+    {
+        double player1Health = 100;//contiene la vida del jugador 1
+        double player2Health = 100;//contiene la vida del jugador 2
+        bool gameOver = false;//sirve para verificar si uno de los jugadores perdio
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void eventos(object sender, EventArgs e)
+        {
+            if(player1Health > 1)//compruebo si alguno de los jugadores sigue con vida
+            {
+                progressBar1.Value = Convert.ToInt32(player1Health);//modifico los valores de la vida de cada jugador
+            }
+            else
+            {
+                gameOver = true;//en caso de que su vida sea menor a 1 ierde el juego
+            }
+
+            if (player2Health > 1)//compruebo si alguno de los jugadores sigue con vida
+            {
+                progressBar2.Value = Convert.ToInt32(player2Health);//modifico los valores de la vida de cada jugador
+            }
+            else
+            {
+                gameOver = true;//en caso de que su vida sea menor a 1 ierde el juego
+            }
+
+
+        }
+    }
+}
