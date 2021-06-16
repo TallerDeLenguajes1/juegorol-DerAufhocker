3d874d20f50d869255e35d43fa09699e5569bd3d
Exequiel Diaz <79394777+DerAufhocker@users.noreply.github.com>
2021-06-16 15:33:59 -0300

    creacion del objeto personajes

--- Tolkinen/Tolkinen/clases/personajeRandom.cs
+++ Tolkinen/Tolkinen/clases/personajeRandom.cs
@@ -6,40 +6,21 @@ using System.Threading.Tasks;
 
 namespace Tolkinen.clases
 {
-    public enum tipoRaza
+    public class crearPersonajeRandom
     {
-        Humano,
-        Enano,
-        Orcos,
-        Troll,
-        Elfo
+        public personajes crearPersonaje()
+        {
+            Random random = new Random();
+            personajes personajeRandom = new personajes(tipoRaza.Enano,"Exe","Gurin",DateTime.Now,73,100);
+            personajeRandom.Velocidad = random.Next(1, 10);
+            personajeRandom.Fuerza = random.Next(1, 10);
+            personajeRandom.Destreza = random.Next(1, 10);
+            personajeRandom.Armadura = random.Next(1, 10);
+            personajeRandom.Nivel = random.Next(1, 10);
+        }
+
     }
 
-    public class personajeRandom
-    {
-        private tipoRaza raza;
-        private string nombre;
-        private string apodo;
-        private DateTime nacimiento;
-        private int edad;
-        private int salud;
-
-        private int velocidad;
-        private int destreza;
-        private int fuerza;
-        private int nivel;
-        private int armadura;
-
-        public tipoRaza Raza { get => raza; set => raza = value; }
-        public string Nombre { get => nombre; set => nombre = value; }
-        public string Apodo { get => apodo; set => apodo = value; }
-        public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
-        public int Edad { get => edad; set => edad = value; }
-        public int Salud { get => salud; set => salud = value; }
-        public int Velocidad { get => velocidad; set => velocidad = value; }
-        public int Destreza { get => destreza; set => destreza = value; }
-        public int Fuerza { get => fuerza; set => fuerza = value; }
-        public int Nivel { get => nivel; set => nivel = value; }
-        public int Armadura { get => armadura; set => armadura = value; }
-    }
 }
+    
+
